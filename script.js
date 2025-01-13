be6dc69939813049751cf61b6e9809dddfdde248
Ayushvik <161769397+Ayushvik@users.noreply.github.com>
2025-01-13 10:59:24 +0530

    git push

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.getElementById('downloadButton').addEventListener('click', function () {
+document.getElementById('downloadLink').addEventListener('click', function() {
+    // URL to the PDF file
+    const fileUrl = 'Resume-ayush.pdf'; // Replace with your actual PDF path
     const link = document.createElement('a');
-    link.href = 'C:\portfolio';
-    link.download = 'Resume-ayush';  // Desired name for the downloaded file
+    link.href = fileUrl;
+    link.download = 'Resume-ayush'; // The name of the downloaded file
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
